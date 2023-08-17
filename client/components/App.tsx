24f443332e6519db456ae60bd3b5450b219fa23d
James Kwon <135941325+james-kwon97@users.noreply.github.com>
2023-08-17 16:19:43 +1200

    Basic component pages added

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,10 @@
+import Nav from './Nav.tsx'
+
 function App() {
   return (
     <div>
       <h1>App</h1>
-      <p>React development has begun!</p>
+      <p>Navigating Book-Keepers' Library!</p>
     </div>
   )
 }
---
+++ client/components/Books.tsx
@@ -0,0 +1,6 @@
+import { useParams } from 'react-router-dom'
+import booksByGenre from '../data/books'
+
+export default function Books() {
+  //etc etc
+}
