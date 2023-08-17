b43984fcc0fe1cd1bcce7d32fb9eb7513ccf841a
James Kwon <135941325+james-kwon97@users.noreply.github.com>
2023-08-18 11:11:14 +1200

    Added nav to app

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,12 +1,14 @@
 // import Books from './Books.tsx'
 // import Home from './Home.tsx'
 import { Outlet } from 'react-router-dom'
+import Nav from './Nav.tsx'
 
 function App() {
   return (
     <div>
       {/* <h1>App</h1>
       <p>React development has begun!</p> */}
+      <Nav />
       <Outlet />
     </div>
   )
