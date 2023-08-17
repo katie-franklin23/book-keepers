c150aff0d01da534687d939dcd7cf12d3a1a7274
zoe <zoeannproudfoot123@gmail.com>
2023-08-18 10:14:50 +1200

    Hi

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,13 @@
-import Nav from './Nav.tsx'
+// import Books from './Books.tsx'
+// import Home from './Home.tsx'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <h1>App</h1>
-      <p>Navigating Book-Keepers' Library!</p>
+      {/* <h1>App</h1>
+      <p>React development has begun!</p> */}
+      <Outlet />
     </div>
   )
 }
--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,11 @@
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import router from './router.tsx'
 
 import App from './components/App.tsx'
 
 document.addEventListener('DOMContentLoaded', () => {
-  createRoot(document.getElementById('app') as HTMLElement).render(<App />)
+  createRoot(document.getElementById('app') as HTMLElement).render(
+    <RouterProvider router={router} />
+  )
 })
