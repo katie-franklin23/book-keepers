3c97fb56d3326e5219bc27d098ee1e2d4e81d3f4
Katie Franklin <134468803+katie-franklin23@users.noreply.github.com>
2023-08-18 13:42:09 +1200

    Stan and Katie adding recommendations files

---
+++ client/components/Recommendations.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+function Recommendation() {
+  const [recommendations, setRecommendation] = useState([
+    { recommendation: 'Title', genre: 'Thriller', author: 'R L Stine' },
+  ])
+
+  function handleAddRecommendation(newRecommendation: Recommendation) {
+    setRecommendation([...recommendations, newRecommendation])
+  }
+
+  const bookArray = recommendations.map((book) => {
+    return (
+      <li key={book}>
+        <p>Recommendation</p>
+        <p>Genre</p>
+        <p>Author</p>
+      </li>
+    )
+  })
+
+  return (
+    <main>
+      <h1>{'Book Recommendation'}</h1>
+
+      <h2>{'Add a book:'}</h2>
+      <AddRecommendationForm onAddRecommendation={handleAddRecommendation} />
+
+      <h2>{'Here are your books: '}</h2>
+      <ul>{bookArray}</ul>
+    </main>
+  )
+}
+
+export default Recommendation
